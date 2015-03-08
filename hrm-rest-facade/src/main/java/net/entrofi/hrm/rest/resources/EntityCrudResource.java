67d79d56617338c063df75bb66f2194bb0621ee9
hcomak <hasan.comak@tav.aero>
2015-03-08 18:05:22 +0200

    test configuration

---
+++ hrm-rest-facade/src/main/java/net/entrofi/hrm/rest/resources/EntityCrudResource.java
@@ -0,0 +1,143 @@
+package net.entrofi.hrm.rest.resources;
+
+import org.springframework.data.domain.Page;
+
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+
+import static net.entrofi.hrm.rest.resources.EntityCrudResource.UriParams.*;
+/**
+ * DOC documentation:type_definition Please provide <b><u>detailed</u></b> class definition.
+ *
+ * @author hcomak
+ * @created 08 Mar 2015
+ */
+public interface EntityCrudResource<T> {
+
+    public static final class UriParams{
+        public static final String PATH_VERSION_EXPRESSION = "/{version: v([0-9]){1}((.)([0-9]){1,2}){0,1}}";
+
+        public static final String PATH_SEGMENT_DELETE = "/delete";
+
+        public static final String PATH_SEGMENT_ITEM = "/item-";
+
+        public static final String PATH_SEGMENT_LIST_ZERO_ONE = "/{list:{list}(0,1)}";
+
+        public static final String PATH_SEGMENT_LIST_ONLY_ONE = "/{list:{list}(1,1)}";
+
+        public static final String QUERY_PARAM_LIMIT = "limit";
+
+        public static final String QUERY_PARAM_LIMIT_DEFAULT = "-1";
+
+        public static final String PATH_PARAM_OFFSET = "{offset}";
+        public static final String PATH_PARAM_OFFSET_NAME = "offset";
+
+        public static final String QUERY_PARAM_ID = "id";
+
+        public static final String QUERY_PARAM_IDS = "ids";
+
+        public static final String PATH_PARAM_ID = "{id}";
+
+        public static final String PATH_PARAM_ID_NAME = "id";
+
+        public static final String PATH_PARAM_VERSION = "version";
+    }
+
+
+    /**
+     *
+     * list DOC documentation:method
+     *
+     * @param limit
+     * @param offset
+     * @return
+     */
+    @SuppressWarnings("PathAnnotation")
+    @GET
+    @Path(value = PATH_SEGMENT_LIST_ZERO_ONE)
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<T> list( @DefaultValue(QUERY_PARAM_LIMIT_DEFAULT) @QueryParam(QUERY_PARAM_LIMIT) int limit, @QueryParam("offset") int offset);
+
+    /**
+     * DOC documentation:method
+     * @param limit
+     * @param offset
+     * @param sort
+     * @return
+     */
+    @SuppressWarnings("PathAnnotation")
+    @GET
+    @Path(PATH_SEGMENT_LIST_ONLY_ONE+"/" + PATH_PARAM_OFFSET)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Page<T> listPage( @DefaultValue(QUERY_PARAM_LIMIT_DEFAULT) @QueryParam(QUERY_PARAM_LIMIT) int limit, @PathParam("offset") int offset, @MatrixParam("sort")String sort);
+
+    /**
+     *
+     * find DOC documentation:method
+     *
+     * @param id
+     * @return
+     */
+    @GET
+    @Path(PATH_SEGMENT_ITEM+PATH_PARAM_ID)
+    public T find(@PathParam(PATH_PARAM_ID_NAME) String id);
+
+    /**
+     *
+     * add DOC documentation:method
+     *
+     * @param entity
+     * @return
+     */
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response add(@Valid T entity);
+
+
+    /**
+     *
+     * addXML DOC documentation:method
+     * @NOTE: This method might not be necessary at this level. Check out in the implementation time.
+     * @param entity
+     * @return
+     */
+    @POST
+    @Consumes(MediaType.APPLICATION_XML)
+    public Response addXML(@Valid T entity);
+
+    /**
+     *
+     * delete DOC documentation:method
+     *
+     * @param id
+     * @return
+     */
+    @DELETE
+    @Path( PATH_SEGMENT_DELETE + PATH_SEGMENT_ITEM + PATH_PARAM_ID)
+    public Response delete( @PathParam(PATH_PARAM_ID_NAME) long id);
+
+    /**
+     *
+     * delete DOC documentation:method
+     *
+     * @param entity
+     * @return
+     */
+    @PUT
+    @Path(PATH_SEGMENT_DELETE)
+    public Response delete(T entity);
+
+
+    /**
+     * DOC documentation:method
+     * @param ids
+     * @return
+     */
+    @DELETE
+    @Path(PATH_SEGMENT_DELETE)
+    public Response delete( @QueryParam(QUERY_PARAM_IDS)List<Long>ids);
+}
---
+++ hrm-rest-facade/src/main/java/net/entrofi/hrm/rest/webapp/HRMApplication.java
@@ -0,0 +1,56 @@
+package net.entrofi.hrm.rest.webapp;
+
+
+import org.glassfish.jersey.filter.LoggingFilter;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
+
+import javax.ws.rs.ApplicationPath;
+
+/*******************************************************************************
+ * KBMSApplication
+ *
+ * DOC documentation:type_definition Please provide <b><u>detailed</u></b> class definition.
+ * Sample Start: This class is a part of KBMS application 
+ * and is responsible of accessing remote machines with the use of different 
+ * protocols.
+ *
+ *
+ * @author hcomak
+ * @created Dec 14, 2014
+ * @version Dec 14, 2014
+ * @since TODO insert the product line
+ * @see <Add inherited classes>
+ ******************************************************************************/
+@ApplicationPath("/")
+public class HRMApplication extends ResourceConfig {
+
+    private final static java.util.logging.Logger LOG = java.util.logging.Logger.getGlobal();
+
+    public HRMApplication(){
+        register(JacksonFeature.class);
+        register(MultiPartFeature.class);
+
+        // Register an instance of LoggingFilter.
+        register(new LoggingFilter(LOG, true));
+
+        // Validation.
+        //register(ValidationConfigurationContextResolver.class);
+
+        //Register app specifics
+//        register(new SortBinder());
+
+        // Enable Tracing support.
+        property(ServerProperties.TRACING, "ALL");
+
+        //Enable sending error in responses
+        property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
+
+        String[] packages = {"net.entrofi.hrm.rest.resources", "net.entrofi.hrm.rest.webapp"};
+        property(ServerProperties.PROVIDER_PACKAGES, packages);
+        packages(true,packages);
+
+    }
+}
